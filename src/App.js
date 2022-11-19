f2106b4934c1627667b5aca4e5d1986fcd7b06d7
isabellexvr <isabellexavier59@gmail.com>
2022-11-19 00:38:30 -0300

    layout: main page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignUpPage from "./HomePages/SignUpPage";
 import WelcomePage from "./HomePages/WelcomePage";
 import TokenProvider from "./contexts/Token";
+import MainPage from "./MainPages/MainPage";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<WelcomePage />} />
           <Route path="/sign-in" element={<SignInPage />} />
           <Route path="/sign-up" element={<SignUpPage />} />
+          <Route path="/main" element={<MainPage />} />
         </Routes>
         <GlobalStyle />
       </BrowserRouter>
--- src/MainPages/MainPage.js
+++ src/MainPages/MainPage.js
@@ -1,7 +1,215 @@
 import styled from "styled-components";
+import { MdExitToApp } from "react-icons/md";
+import { AiOutlinePlusCircle, AiOutlineMinusCircle } from "react-icons/ai";
+import { useState } from "react";
 
-export default function MainPage(){
-    return (
-        <div>oi!</div>
-    )
+const entrysAndOutputs = [
+  { date: "24/11", description: "Empréstimo", price: "500,00", type: "entry" },
+  { date: "20/11", description: "Almoço mãe", price: "39,90", type: "output" },
+  { date: "10/11", description: "Almoço só", price: "29,90", type: "output" },
+];
+
+export default function MainPage() {
+  const [registries, setRegistries] = useState(["a"]);
+  return (
+    <>
+      {!registries && (
+        <PageContainer>
+          <HeaderStyle>
+            <h1>Olá, Fulano</h1>
+            <MdExitToApp />
+          </HeaderStyle>
+          <RegistriesStyle display={"flex"}>
+            <h1>Não há registros de entrada ou saída</h1>
+          </RegistriesStyle>
+
+          <ButtonsContainer>
+            <div className="new-entry">
+              <AiOutlinePlusCircle />
+              <h1>Nova entrada</h1>
+            </div>
+            <div className="new-output">
+              <AiOutlineMinusCircle />
+              <h1>Nova saída</h1>
+            </div>
+          </ButtonsContainer>
+        </PageContainer>
+      )}
+      {registries && (
+        <PageContainer>
+          <HeaderStyle>
+            <h1>Olá, Fulano</h1>
+            <MdExitToApp />
+          </HeaderStyle>
+          <RegistriesStyle paddingTop={"23px"} display={"initial"}>
+            {entrysAndOutputs.map((t) => (
+              <RegistryStyle>
+                <div>
+                  <h2>{t.date}</h2>
+                  <p>{t.description}</p>
+                </div>
+
+                <Price color={t.type === "entry" ? "#03AC00" : "#C70000"}>
+                  {t.price}
+                </Price>
+              </RegistryStyle>
+            ))}
+            <Balance>
+              <h1>SALDO</h1>
+              <p>21312,12</p>
+            </Balance>
+          </RegistriesStyle>
+          <ButtonsContainer>
+            <div className="new-entry">
+              <AiOutlinePlusCircle />
+              <h1>Nova entrada</h1>
+            </div>
+            <div className="new-output">
+              <AiOutlineMinusCircle />
+              <h1>Nova saída</h1>
+            </div>
+          </ButtonsContainer>
+        </PageContainer>
+      )}
+    </>
+  );
 }
+
+const HeaderStyle = styled.div`
+  height: 78px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 87%;
+  > h1 {
+    font-family: "Raleway";
+    font-weight: 700;
+    font-size: 26px;
+    color: #ffffff;
+  }
+  > svg {
+    color: white;
+    font-size: 30px;
+  }
+`;
+
+const PageContainer = styled.div`
+  background-color: #915fbf;
+  align-items: center;
+  height: 177.5vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const RegistriesStyle = styled.div`
+  position: relative;
+  width: 87%;
+  height: 446px;
+  border-radius: 5px;
+  background: #ffffff;
+  display: ${(props) => props.display};
+  padding-top: ${(props) => props.paddingTop};
+  box-sizing: border-box;
+  justify-content: center;
+  align-items: center;
+  > h1 {
+    color: #868686;
+    font-family: "Raleway";
+    font-weight: 400;
+    font-size: 20px;
+    line-height: 23px;
+    text-align: center;
+    width: 180px;
+  }
+`;
+
+const ButtonsContainer = styled.div`
+  margin-top: 13px;
+  width: 87%;
+  display: flex;
+  justify-content: space-between;
+  > div {
+    height: 114px;
+    width: 47.54%;
+    background-color: #a328d6;
+    border-radius: 5px;
+    box-sizing: border-box;
+    > svg {
+      margin-top: 10.5px;
+      margin-left: 9.56px;
+      font-size: 26px;
+      color: white;
+    }
+    > h1 {
+      margin-top: 24px;
+      margin-left: 10px;
+      width: 64px;
+      font-family: "Raleway";
+      font-weight: 700;
+      font-size: 17px;
+      line-height: 20px;
+      color: #ffffff;
+    }
+  }
+`;
+
+const RegistryStyle = styled.div`
+  margin-bottom: 18px;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  > div {
+    display: flex;
+    > h2 {
+      font-family: "Raleway";
+      font-weight: 400;
+      font-size: 16px;
+      color: #c6c6c6;
+      margin-left: 10px;
+      margin-right: 10px;
+    }
+    > p {
+      font-family: "Raleway";
+      font-weight: 400;
+      font-size: 16px;
+      color: #000000;
+      text-align: left;
+      width: 196px;
+    }
+  }
+`;
+
+const Price = styled.h3`
+  margin-right: 10px;
+  color: ${(props) => props.color};
+  font-family: "Raleway";
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 19px;
+  text-align: right;
+`;
+
+const Balance = styled.div`
+  display: flex;
+  width: 93%;
+  justify-content: space-between;
+  position: absolute;
+  bottom: 10px;
+  right: 12px;
+  > h1 {
+    font-family: "Raleway";
+    font-weight: 700;
+    font-size: 17px;
+    color: #000000;
+  }
+  > p {
+    font-family: "Raleway";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 17px;
+    text-align: right;
+    color: #03ac00;
+  }
+`;
